8ffc3a4efd3187e7f1fbf6c4a2b8041429d6882e
ohanamitanoshii <reia.1004@icloud.com>
2024-08-05 06:09:16 +1000

    Add files via upload

---
+++ wiki/encyclopedia/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+from . import util
+
+
+def index(request):
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries()
+    })
+
+def entry(request):
+    return render(request, "encyclopedia/entry.html",{
+        "entry": util.get_entry()
+    })
